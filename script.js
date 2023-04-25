674d233af9e1887e85e4dee847db8eca41edc46e
PedroQDV <pedro.dozzi10@gmail.com>
2023-04-25 15:43:58 -0300

    Correções de erros header

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-/*=============== scroll sections active link ===============*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
-
-/*=============== scroll sections active link ===============*/
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-
-    /*=============== sticky navbar ===============*/
-    let header = document.querySelector('header');
-
-    header.classList.toggle('stick', window.scrollY > 100);
-
-    /*===== remove toggle icon and navbar when click navbar link =====*/
-
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-
-};
-
 /*=============== scroll reveal ===============*/
 ScrollReveal({
     reset: true,
